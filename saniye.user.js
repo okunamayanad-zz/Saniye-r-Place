dddcf61a5347c7d9a5157434fab58ffacb3784f6
DeadLyBro <31214077+DeadLyBro@users.noreply.github.com>
2022-04-04 13:17:23 +0300

    Update saniye.user.js

--- saniye.user.js
+++ saniye.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://i.hizliresim.com/it4qdv2.png";
+        image.src = "https://i.hizliresim.com/dy6773k.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
